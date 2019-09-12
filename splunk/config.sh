ddc722fdbf5838a3ca63e4d6bb1b916280128f8f
Cribl CI <criblci@cribl.io>
2019-09-12 01:18:25 +0000

    Fixing Scope (#10)
    * in progress scope commit
    
    * in progress commit
    
    * updating to use environment config, re-enabling for splunk
    
    * removing privilege

--- splunk/config.sh
+++ splunk/config.sh
@@ -4,5 +4,8 @@ if [ -n "$CRIBL_ROUTING_DEMO" ]; then
 	cat <<-EOF >> $SPLUNK_HOME/etc/apps/cribl/local/inputs.conf
 		[splunktcp://9997]
 		connection_host = ip
+
+		[splunktcp://9998]
+		connection_host = ip
 	EOF
 fi
